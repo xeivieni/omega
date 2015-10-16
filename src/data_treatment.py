fe3604409413ce811361c0f7a760c27923aa9e80
xeivieni <clement.mondion@gmail.com>
2015-10-16 13:41:48 +0200

    All formulas handled in calculator class. Computations are automatically run from the constructor. Formulas : avgs, moments and central moments, var, dissym and flattening

--- src/data_treatment.py
+++ src/data_treatment.py
@@ -31,7 +31,7 @@ def moments(data):
     mom_cent_4 = u4 / data[0]
     sqrt_var = sqrt(mom_cent_2)
 
-    #Coefficient of asymmetry and flattening
+    # Coefficient of asymmetry and flattening
     y1 = (mom_cent_3 / (sqrt_var ** 3))
     y2 = (mom_cent_4 / (sqrt_var ** 4)) - 3
     ##########################################################################################
@@ -65,7 +65,7 @@ class Calculator(object):
         self.dissym = 0
         self.min = 0
         self.max = 0
-
+        self.flattening = 0
 
     def display_results(self):
         """
@@ -73,25 +73,35 @@ class Calculator(object):
         :return:
         """
         print "Resultats pour le fichier : \n================================"
-        print "Moyenne arithmetique", self.arithAvg
-        print "Moyenne quadratique", self.quadAvg
-        print "Moyenne geometrique", self.geoAvg
-        print "Moyenne harmonique", self.harmAvg
-        print "Ecart a la moyenne", self.std
-        print "Valeure maximale", self.max
-        print "Valeurs minimale", self.min
-        print "Variance", self.var
-        print "Moment d'ordre R (defaut 1)", self.momentsR
-        print "Moment centre d'ordre R (default 1)", self.centralMomentsR
-        print "Dissymetrie", self.dissym
+        print "Moyenne arithmetique : ", self.arithAvg
+        print "Moyenne quadratique : ", self.quadAvg
+        print "Moyenne geometrique : ", self.geoAvg
+        print "Moyenne harmonique : ", self.harmAvg
+        print "Ecart a la moyenne : ", self.std
+        print "Valeure maximale : ", self.max
+        print "Valeurs minimale : ", self.min
+        print "Variance : ", self.var
+        print "Moment d'ordre R (defaut 1) : ", self.momentsR
+        print "Moment centre d'ordre R (default 1) : ", self.centralMomentsR
+        print "Dissymetrie : ", self.dissym
+        print "Coefficient d'applatissement : ", self.flattening
 
     def average(self, data, number):
-        return numpy.sum(data)/number
+        return numpy.sum(data) / number
 
     def moments(self, data, occurrence, order):
-        return self.average([occurrence[i]*(data[i]**order) for i in range(len(data))], len(data))
+        _moments = []
+        for j in range(order):
+            _moments.append(
+                self.average([occurrence[i] * (data[i] ** j + 1) for i in range(len(data))], len(data)))
+        return _moments
 
-        #TODO : Create average functions in parent class and manipulated data in children
+    def coefficients(self):
+        if len(self.centralMomentsR) > 2:
+            self.var = math.sqrt(
+                self.centralMomentsR[1])  # Variance is the square root of the central moment of order 2
+            self.dissym = self.centralMomentsR[2] / (self.var ** 3)
+            self.flattening = (self.centralMomentsR[3] / (self.var ** 4)) - 3
 
 
 class NonGroupedDiscrete(Calculator):
@@ -101,28 +111,21 @@ class NonGroupedDiscrete(Calculator):
         Calculator.__init__(self)
         self.nbLines = n
         self.data = d
+        self.run()
 
     def run(self):
         self.arithAvg = self.average(self.data, self.nbLines)
         self.quadAvg = math.sqrt(self.average([i * i for i in self.data], self.nbLines))
         self.geoAvg = math.exp(self.average([numpy.log(i) for i in self.data], self.nbLines))
-        self.harmAvg = 1/self.average([(1 / self.data[i]) for i in range(len(self.data))], self.nbLines)
+        self.harmAvg = 1 / self.average([(1 / self.data[i]) for i in range(len(self.data))], self.nbLines)
         self.max = numpy.max(self.data)
         self.min = numpy.min(self.data)
-        self.momentsR.append(self.moments(self.data, [1 for i in range(len(self.data))], 1))
+        self.momentsR = self.moments(self.data, [1 for i in range(len(self.data))], 4)
+        self.centralMomentsR = self.moments([(i - self.arithAvg) for i in self.data],
+                                            [1 for i in range(len(self.data))], 4)
+        self.std = self.average([abs(i - self.arithAvg) for i in self.data], self.nbLines)
 
-    def standard_deviation(self):
-        dev_data = [abs(i - self.arithAvg) for i in self.data]
-        self.std = numpy.sum(dev_data) / self.nbLines
-        print "Ecart a la moyenne arithmetique discrete non groupee = ", self.std
 
-    def central_moments(self, r):
-        if self.momentsR==[]:
-            return
-
-        for i in range(r):
-            self.centralMomentsR.append(sum([(j-self.arithAvg)**(i+1) for j in self.data])/self.nbLines)
-            print "Moment centre d'ordre %f = %f" %(i+1, self.centralMomentsR[i])
 
 class GroupedDiscrete(Calculator):
     """Class for grouped discrete values treatment"""
@@ -133,32 +136,27 @@ class GroupedDiscrete(Calculator):
         self.data = d
         self.occurrences = e
         self.totalOccurrences = s
+        self.run()
 
     def run(self):
-        self.arithAvg = self.average([self.data[i]*self.occurrences[i] for i in range(len(self.data))],
+        self.arithAvg = self.average([self.data[i] * self.occurrences[i] for i in range(len(self.data))],
                                      self.totalOccurrences)
-        self.quadAvg = math.sqrt(self.average([(self.data[i] * self.data[i]) * self.occurrences[i] for i in range(len(self.data))],
-                                              self.totalOccurrences))
-        self.geoAvg = math.exp(self.average([numpy.log(self.data[i]) * self.occurrences[i] for i in range(len(self.data))], self.totalOccurrences))
-        self.harmAvg = 1/self.average([(self.occurrences[i] / self.data[i]) for i in range(len(self.data))],
-                                      self.totalOccurrences)
+        self.quadAvg = math.sqrt(
+            self.average([(self.data[i] * self.data[i]) * self.occurrences[i] for i in range(len(self.data))],
+                         self.totalOccurrences))
+        self.geoAvg = math.exp(
+            self.average([numpy.log(self.data[i]) * self.occurrences[i] for i in range(len(self.data))],
+                         self.totalOccurrences))
+        self.harmAvg = 1 / self.average([(self.occurrences[i] / self.data[i]) for i in range(len(self.data))],
+                                        self.totalOccurrences)
         self.max = numpy.max(self.data)
         self.min = numpy.min(self.data)
-        self.momentsR.append(self.moments(self.data, self.occurrences, 1))
+        self.momentsR = self.moments(self.data, self.occurrences, 4)
+        self.centralMomentsR = self.moments([(i - self.arithAvg) for i in self.data], self.occurrences, 4)
+        self.std = self.average(
+            [self.occurrences[i] * abs(self.data[i] - self.arithAvg) for i in range(len(self.data))],
+            self.totalOccurrences)
 
-    def standard_deviation(self):
-        dev_data = [self.occurrences[i] * abs(self.data[i] - self.arithAvg) for i in range(len(self.data))]
-        self.std = numpy.sum(dev_data) / self.totalOccurrences
-        print "Ecart a la moyenne arithmetique discrete groupee = ", self.std
-
-    def central_moments(self, r):
-        if self.momentsR==[]:
-            return
-
-        for i in range(r):
-            self.centralMomentsR.append(sum([self.occurrences[j]*((self.data[j]-self.arithAvg)**(i+1))
-                                             for j in range(len(self.data))])/self.totalOccurrences)
-            print "Moment centre d'ordre %f = %f" %(i+1, self.centralMomentsR[i])
 
 
 class NonGroupedContinuous(Calculator):
@@ -168,28 +166,20 @@ class NonGroupedContinuous(Calculator):
         Calculator.__init__(self)
         self.nbLines = n
         self.data = d
+        self.run()
 
     def run(self):
         self.arithAvg = self.average(self.data, self.nbLines)
         self.quadAvg = math.sqrt(self.average([i * i for i in self.data], self.nbLines))
         self.geoAvg = math.exp(self.average([numpy.log(i) for i in self.data], self.nbLines))
-        self.harmAvg = 1/self.average([(1 / self.data[i]) for i in range(len(self.data))], self.nbLines)
+        self.harmAvg = 1 / self.average([(1 / self.data[i]) for i in range(len(self.data))], self.nbLines)
         self.max = numpy.max(self.data)
         self.min = numpy.min(self.data)
-        self.momentsR.append(self.moments(self.data, [1 for i in range(len(self.data))], 1))
+        self.momentsR = self.moments(self.data, [1 for i in range(len(self.data))], 4)
+        self.centralMomentsR = self.moments([(i - self.arithAvg) for i in self.data],
+                                            [1 for i in range(len(self.data))], 4)
+        self.std = self.average([abs(i - self.arithAvg) for i in self.data], self.nbLines)
 
-    def standard_deviation(self):
-        dev_data = [abs(i - self.arithAvg) for i in self.data]
-        self.std = numpy.sum(dev_data) / self.nbLines
-        print "Ecart a la moyenne arithmetique continues non groupee = ", self.std
-
-    def central_moments(self, r):
-        if self.momentsR==[]:
-            return
-
-        for i in range(r):
-            self.centralMomentsR.append(sum([(j-self.arithAvg)**(i+1) for j in self.data])/self.nbLines)
-            print "Moment centre d'ordre %f = %f" %(i+1, self.centralMomentsR[i])
 
 
 class GroupedContinuous(Calculator):
@@ -202,30 +192,25 @@ class GroupedContinuous(Calculator):
         self.higherBounds = h
         self.centers = [(self.lowerBounds[i] + self.higherBounds[i]) / 2 for i in range(len(self.lowerBounds))]
         self.occurrences = e
+        self.run()
 
     def run(self):
         self.arithAvg = self.average([self.centers[i] * self.occurrences[i] for i in range(len(self.centers))],
                                      sum(self.occurrences))
-        self.quadAvg = math.sqrt(self.average([(self.centers[i] * self.centers[i]) * self.occurrences[i] for i in range(len(self.centers))],
-                                              sum(self.occurrences)))
+        self.quadAvg = math.sqrt(
+            self.average([(self.centers[i] * self.centers[i]) * self.occurrences[i] for i in range(len(self.centers))],
+                         sum(self.occurrences)))
 
-        self.geoAvg = math.exp(self.average([numpy.log(self.centers[i]) * self.occurrences[i] for i in range(len(self.centers))], sum(self.occurrences)))
-        self.harmAvg = 1/self.average([(self.occurrences[i] / self.centers[i]) for i in range(len(self.centers))],
-                                      sum(self.occurrences))
+        self.geoAvg = math.exp(
+            self.average([numpy.log(self.centers[i]) * self.occurrences[i] for i in range(len(self.centers))],
+                         sum(self.occurrences)))
+        self.harmAvg = 1 / self.average([(self.occurrences[i] / self.centers[i]) for i in range(len(self.centers))],
+                                        sum(self.occurrences))
         self.max = numpy.max(self.centers)
         self.min = numpy.min(self.centers)
-        self.momentsR.append(self.moments(self.centers, self.occurrences, 1))
+        self.momentsR = self.moments(self.centers, self.occurrences, 4)
+        self.centralMomentsR = self.moments([(i - self.arithAvg) for i in self.centers], self.occurrences, 4)
+        self.std = self.average(
+            [self.occurrences[i] * abs(self.centers[i] - self.arithAvg) for i in range(len(self.centers))],
+            numpy.sum(self.occurrences))
 
-    def standard_deviation(self):
-        dev_data = [self.occurrences[i] * abs(self.centers[i] - self.arithAvg) for i in range(len(self.centers))]
-        self.std = numpy.sum(dev_data) / numpy.sum(self.occurrences)
-        print "Ecart a la moyenne arithmetique continue groupee = ", self.std
-
-    def central_moments(self, r):
-        if self.momentsR==[]:
-            return
-
-        for i in range(r):
-            self.centralMomentsR.append(sum([self.occurrences[j]*((self.centers[j]-self.arithAvg)**(i+1))
-                                             for j in range(len(self.centers))])/len(self.occurrences))
-            print "Moment centre d'ordre %f = %f" %(i+1, self.centralMomentsR[i])
