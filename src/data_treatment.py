f6022af7ae1be58298a4cfb3b59aed2fb6e5ad85
xeivieni <clement.mondion@gmail.com>
2016-01-24 15:18:57 +0100

    Project now asks for mu, sigma, k, n values
    Before estimating the parameters, the user can enter them manually

--- src/data_treatment.py
+++ src/data_treatment.py
@@ -9,8 +9,6 @@ import tex_generator
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
-#TODO => Ecart type = sqrt(var)
-
 
 class Calculator(object):
     """
@@ -22,7 +20,8 @@ class Calculator(object):
         """
         Class constructor for non grouped discrete data
         """
-        self.types = ["non group\\'ees discr\\`etes", "non group\\'ees continues", "group\\'ees disc\\`etes", "group\\'ees continues"]
+        self.types = ["non group\\'ees discr\\`etes", "non group\\'ees continues", "group\\'ees disc\\`etes",
+                      "group\\'ees continues"]
         self.results = {'arithAvg': 0,
                         'quadAvg': 0,
                         'geoAvg': 0,
@@ -86,12 +85,6 @@ class Calculator(object):
         self.histogram()
         self.generate_latex()
 
-        X = self.Normale_Rup(10, 7, 2, 50, 100)
-        #print X
-        Y = self.Normale_Rup_Estim(X)
-        #print Y
-
-
     def calculate(self):
         """
         This method runs the different operations in order to calculate the
@@ -105,23 +98,23 @@ class Calculator(object):
         self.results['arithAvg'] = self.average([self.data[i] * self.occurrences[i] for i in range(len(self.data))],
                                                 self.totalOccurrences)
         self.results['quadAvg'] = math.sqrt(
-            self.average([(self.data[i] * self.data[i]) * self.occurrences[i] for i in range(len(self.data))],
-                         self.totalOccurrences))
+                self.average([(self.data[i] * self.data[i]) * self.occurrences[i] for i in range(len(self.data))],
+                             self.totalOccurrences))
         if self.results['min'] > 0:
             self.results['geoAvg'] = math.exp(
-                self.average([numpy.log(self.data[i]) * self.occurrences[i] for i in range(len(self.data))],
-                             self.totalOccurrences))
+                    self.average([numpy.log(self.data[i]) * self.occurrences[i] for i in range(len(self.data))],
+                                 self.totalOccurrences))
             self.results['harmAvg'] = 1 / self.average(
-                [(self.occurrences[i] / self.data[i]) for i in range(len(self.data))],
-                self.totalOccurrences)
+                    [(self.occurrences[i] / self.data[i]) for i in range(len(self.data))],
+                    self.totalOccurrences)
         else:
             self.results['geoAvg'] = self.results['harmAvg'] = "N/A"
         self.results['momentsR'] = self.moments(self.data, self.occurrences, 4)
         self.results['centralMomentsR'] = self.moments([(i - self.results['arithAvg']) for i in self.data],
                                                        self.occurrences, 4)
         self.results['std'] = self.average(
-            [self.occurrences[i] * abs(self.data[i] - self.results['arithAvg']) for i in range(len(self.data))],
-            self.totalOccurrences)
+                [self.occurrences[i] * abs(self.data[i] - self.results['arithAvg']) for i in range(len(self.data))],
+                self.totalOccurrences)
 
     def display_results(self):
         """
@@ -161,7 +154,7 @@ class Calculator(object):
         _moments = []
         for j in range(order):
             _moments.append(
-                self.average([occurrence[i] * (data[i] ** (j + 1)) for i in range(len(data))], len(data)))
+                    self.average([occurrence[i] * (data[i] ** (j + 1)) for i in range(len(data))], len(data)))
         return _moments
 
     def coefficients(self):
@@ -185,26 +178,18 @@ class Calculator(object):
 
         for i in range(len(self.data)):
             plt.bar(self.data[i], self.occurrences[i])
-            ax.text(self.data[i], self.occurrences[i]+0.1, '%d' % (self.occurrences[i]),
+            ax.text(self.data[i], self.occurrences[i] + 0.1, '%d' % (self.occurrences[i]),
                     horizontalalignment='center',
                     verticalalignment='center', fontsize=12)
 
-        plt.ylim([0, numpy.max(self.occurrences)+0.5])
-        plt.xlim([numpy.min(self.data)-0.2, numpy.max(self.data)+0.2])
+        plt.ylim([0, numpy.max(self.occurrences) + 0.5])
+        plt.xlim([numpy.min(self.data) - 0.2, numpy.max(self.data) + 0.2])
         plt.xticks([k for k in self.data])
         plt.yticks([])
         plt.ylabel('Effectifs', size=16)
         plt.xlabel('Valeurs', size=16)
-
         plt.savefig("histo.png")
 
-        """
-        n, bins, patches = plt.hist(self.data, bins=20, normed=True)
-        plt.xlabel("Observations")
-        plt.ylabel("Effectifs")
-        plt.title("Histogramme")
-        plt.savefig("histo.png")
-        """
     def generate_latex(self):
         print("generating pdf and html files")
         tex_content = tex_generator.TEMPLATE.format(d=self.results, m1=self.results['momentsR'][0],
@@ -238,7 +223,26 @@ class Calculator(object):
             o.append(self.data.count(i))
         self.data = d
         self.occurrences = o
-        print self.data
-        print self.occurrences
 
 
+"""
+<<<<<<< Updated upstream
+
+
+=======
+    def estimation(self):
+        k = 0
+        Sh = 0
+        Shc = 0
+        Ac =
+        Bc = (- self.totalOccurrences - k)/2
+        c = - Ac / Bc
+        As =
+        Bs = - self.totalOccurrences/2
+        sigma = - As / Bs
+        Sh = sum(self.data[:k])
+        Shc = sum(self.data[k+1:])
+        mu = ((-Sh / (sigma ** 2)) - (Shc / (c ** 2 * sigma ** 2))) / (
+             (-k / sigma ** 2) - ((n - k) / c ** 2 * sigma ** 2))
+>>>>>>> Stashed changes
+"""
--- src/projet.py
+++ src/projet.py
@@ -8,98 +8,90 @@ import input_reading
 import math
 import numpy
 
-#TODO : Afficher nombre de donnees dans le fichier pdf
 
-def Normale_Rup(mu,sigma21,sigma22,k,taille):
+# TODO : Afficher nombre de donnees dans le fichier pdf
 
-        X=[]
-        # pour la simulation de la loi normale on nutilise Numpy
+def Normale_Rup(mu, sigma21, sigma22, k, taille):
+    X = []
+    # pour la simulation de la loi normale on nutilise Numpy
 
-        sigma1=math.sqrt(sigma21)           #sigma1 = sigma carre (sujet)
-        sigma2=math.sqrt(sigma22)           #sigma2 = sigma carre * c carre (sujet)
+    sigma1 = math.sqrt(sigma21)  # sigma1 = sigma carre (sujet)
+    sigma2 = math.sqrt(sigma22)  # sigma2 = sigma carre * c carre (sujet)
 
-        for i in range(taille):
-            if i < k:
-                X.append(numpy.random.normal(mu,sigma1))            #Termes de 1 à k
+    for i in range(taille):
+        if i < k:
+            X.append(numpy.random.normal(mu, sigma1))  # Termes de 1 à k
 
-            else:
-                X.append(numpy.random.normal(mu,sigma2))            #Termes de k+1 a n
+        else:
+            X.append(numpy.random.normal(mu, sigma2))  # Termes de k+1 a n
 
-        return X
+    return X
 
 
 def Normale_Rup_Estim(data):
+    n = len(data)
+    V = [0] * len(data)  # La vraisemblance en fonction de k
+    c = [0] * len(data)
+    sigma = [0] * len(data)
+    diff = [0] * len(data)
 
-    n=len(data)
-    V=[0]*len(data)    # La vraisemblance en fonction de k
-    c=[0]*len(data)
-    sigma=[0]*len(data)
-    diff=[0]*len(data)
+    mu = numpy.mean(data)
 
-    mu=numpy.mean(data)
+    diff = [(data[j] - mu) for j in range(len(data))]
+    diff_carre = [diff[l] * diff[l] for l in range(len(diff))]
+    c_carre = list
 
-    diff=[(data[j]-mu) for j in range(len(data))]
-    #print diff
-    diff_carre = [diff[l]*diff[l] for l in range(len(diff))]
-    #print diff_carre
+    for i in range(1, len(data) - 1):
+        c[i] = math.sqrt(numpy.abs(
+            float(float(numpy.sum((data[i + 1:n]))) - n * mu + i * mu) / float(i * mu - float(numpy.sum((data[:i]))))))
+        c_carre = [c[m] * c[m] for m in range(len(c))]
 
-    for i in range(1,len(data)-1):
+        sigma[i] = float(numpy.sum(diff_carre[:i]) + (float(1 / (c_carre[i])) * float(numpy.sum(diff_carre[i + 1:n]))))
+        sigma_carre = [sigma[o] * sigma[o] for o in range(len(sigma))]
 
-        c[i]=math.sqrt(numpy.abs(float(float(numpy.sum((data[i+1:n])))-n*mu+i*mu)/        float(i*mu-float(numpy.sum((data[:i]))) )))
-        c_carre= [c[m]*c[m] for m in range(len(c))]
+        V[i] = numpy.abs(
+            float(-n / 2) * math.log(float(2 * math.pi)) + float(-n / 2) * math.log(float(sigma_carre[i])) - float(
+                (n - i) / 2) * math.log(float(c_carre[i])) - float(1 / (2 * sigma_carre[i])) * float(
+                numpy.sum((diff_carre[:i]))) - float(1 / (2 * c_carre[i] * sigma_carre[i])) * float(
+                numpy.sum((diff_carre[i + 1:n]))))
 
-        sigma[i]=float(numpy.sum(diff_carre[:i]) + (float(1/(c_carre[i]))*float(numpy.sum(diff_carre[i+1:n]))   ))
-        sigma_carre=[sigma[o]*sigma[o] for o in range(len(sigma))]
-
-        V[i]= numpy.abs(float(-n/2)*math.log(float(2*math.pi))    +      float(-n/2)*math.log(float(sigma_carre[i]))     -      float((n-i)/2)*math.log(float(c_carre[i]))       -      float(1/(2*sigma_carre[i]))*float(numpy.sum((diff_carre[:i])))           -             float(1/(2*c_carre[i]*sigma_carre[i]))*float(numpy.sum((diff_carre[i+1:n]))))
-
-
-
-
-    k=numpy.argmax(V)  # l'indice max du Vraisemblance
+    k = numpy.argmax(V)  # l'indice max du Vraisemblance
     c_estim = c_carre[k]
-    sigma_estim=sigma[k]
+    sigma_estim = sigma[k]
 
-    #print V
-
-    #print "k = ",k
-    #print "mu = ",mu
-    #print "c = ",c_estim
-    #print "sigma = ",sigma_estim
-
-
-    return k,mu,c_estim,sigma_estim
-
+    return k, mu, c_estim, sigma_estim
 
 
 if __name__ == '__main__':
-    # fichiers a 1-colonne
 
+    print "Veuillez entrer les paramètres"
+    mu = float(raw_input("mu : "))
+    sigma21 = float(raw_input("sigma 1 : "))
+    sigma22 = float(raw_input("sigma 2 : "))
+    k = int(raw_input("k : "))
+    n = int(raw_input("n : "))
 
-    fichier_k= open('Estimateur_k.dat', 'w')
-    fichier_mu= open('mu.dat', 'w')
-    fichier_c= open('c.dat', 'w')
-    fichier_sigma= open('sigma.dat', 'w')
+    fichier_k = open('Estimateur_k.dat', 'w')
+    fichier_mu = open('mu.dat', 'w')
+    fichier_c = open('c.dat', 'w')
+    fichier_sigma = open('sigma.dat', 'w')
 
-    M=200
+    M = 200
     for i in range(M):
-        X = Normale_Rup(10, 7, 2, 50, 100)
-        #print X
+        X = Normale_Rup(mu, sigma21, sigma22, k, n)
+
         Y = Normale_Rup_Estim(X)
 
-        fichier_k.write("%d\n" %Y[0])
-        fichier_mu.write("%0.4f\n" %Y[1])
-        fichier_c.write("%0.4f\n" %Y[2])
-        fichier_sigma.write("%0.4f\n" %Y[3])
-
+        fichier_k.write("%d\n" % Y[0])
+        fichier_mu.write("%0.4f\n" % Y[1])
+        fichier_c.write("%0.4f\n" % Y[2])
+        fichier_sigma.write("%0.4f\n" % Y[3])
 
     fichier_k.close()
     fichier_mu.close()
     fichier_c.close()
     fichier_sigma.close()
 
-
-    #print('file name is : '), fichier_k
     s = input_reading.Lecture_data("Estimateur_k.dat")
     if len(s) == 3:
         data_set = data_treatment.Calculator(n=s[0], d=s[1], o=s[2])
@@ -107,6 +99,3 @@ if __name__ == '__main__':
         data_set = data_treatment.Calculator(n=s[0], dl=s[1], dr=s[2], o=s[3])
     else:
         data_set = data_treatment.Calculator(n=s[0], d=s[1])
-
-
-
